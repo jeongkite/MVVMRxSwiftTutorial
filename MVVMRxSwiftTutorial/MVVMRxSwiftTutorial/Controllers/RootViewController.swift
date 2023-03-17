f1c65def51e7e040803798455732e2d6d8027b62
Jeongyeon Yang <jeong6kite@gmail.com>
2023-03-17 18:48:47 +0900

    Feat collectionview 구현

--- MVVMRxSwiftTutorial/MVVMRxSwiftTutorial/Controllers/RootViewController.swift
+++ MVVMRxSwiftTutorial/MVVMRxSwiftTutorial/Controllers/RootViewController.swift
@@ -15,9 +15,17 @@ class RootViewController: UIViewController {
     let disposeBag = DisposeBag()
     let viewModel: RootViewModel
     
-    private let articles = BehaviorRelay<[Article]>(value: [])
-    var articlesObserver: Observable<[Article]> {
-        return articles.asObservable()
+    private lazy var collectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: UICollectionViewLayout())
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .blue
+        return collectionView
+    }()
+    
+    private let articleViewModel = BehaviorRelay<[ArticleViewModel]>(value: [])
+    var articleViewModelObserver: Observable<[ArticleViewModel]> {
+        return articleViewModel.asObservable()
     }
     
     // MARK: Lifecycles
@@ -34,6 +42,7 @@ class RootViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        configureCollectionView()
         configureUI()
         fetchArticles()
         subscribe()
@@ -41,20 +50,49 @@ class RootViewController: UIViewController {
     
     // MARK: Configures
     func configureUI() {
-        view.backgroundColor = .systemBackground
+        view.backgroundColor = .red
+        
+        view.addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        collectionView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor).isActive = true
+        collectionView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+    func configureCollectionView() {
+        self.collectionView.register(ArticleCollectionViewCell.self, forCellWithReuseIdentifier: "cell")
     }
 
     // MARK: Helpers
     func fetchArticles() {
-        self.viewModel.fetchArticles().subscribe(onNext: { articles in
-            self.articles.accept(articles)
+        viewModel.fetchArticles().subscribe(onNext: { articleViewModel in
+            self.articleViewModel.accept(articleViewModel)
         }).disposed(by: disposeBag)
     }
     
     func subscribe() {
-        self.articlesObserver.subscribe(onNext: { articles in
-            // collectionView 생성 예정, 이때 reloadData() 필요
-        }).disposed(by: disposeBag)
+        self.articleViewModelObserver
+            .observe(on: MainScheduler.instance)
+            .subscribe(onNext: { articles in
+                print(articles)
+                self.collectionView.reloadData()
+            }).disposed(by: disposeBag)
+    }
+}
+
+extension RootViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.articleViewModel.value.count
     }
     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! ArticleCollectionViewCell
+        let articleViewModel = self.articleViewModel.value[indexPath.row]
+        cell.viewModel.onNext(articleViewModel)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 120)
+    }
 }
---
+++ MVVMRxSwiftTutorial/MVVMRxSwiftTutorial/ViewModels/ArticleViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  ArticleViewModel.swift
+//  MVVMRxSwiftTutorial
+//
+//  Created by 양정연 on 2023/03/17.
+//
+
+import Foundation
+
+struct ArticleViewModel {
+    private let article: Article
+    
+    var imageUrl: String? {
+        return article.urlToImage
+    }
+    
+    var title: String? {
+        return article.title
+    }
+    
+    var description: String? {
+        return article.description
+    }
+    
+    init(article: Article) {
+        self.article = article
+    }
+}
