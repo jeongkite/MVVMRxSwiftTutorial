77afd414b437b8881e753a8192ae10384570672a
Jeongyeon Yang <jeong6kite@gmail.com>
2023-03-17 13:38:28 +0900

    Create 프로젝트 생성

---
+++ MVVMRxSwiftTutorial/MVVMRxSwiftTutorial/Controllers/RootViewController.swift
@@ -0,0 +1,28 @@
+//
+//  RootViewController.swift
+//  MVVMRxSwiftTutorial
+//
+//  Created by 홍주은 on 2023/03/17.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    
+    // MARK: Lifecycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        configureUI()
+    }
+    
+    // MARK: Configures
+    func configureUI() {
+        
+    }
+
+}
